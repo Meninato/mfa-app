3269115daae66224ce3f484f781bbcb528a9de65
Fernando Meninato <meninato@protonmail.com>
2023-09-23 23:28:34 -0300

    core layout started

--- mfa-app/src/app/app.module.ts
+++ mfa-app/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ToastrModule } from 'ngx-toastr';
+import { HeaderComponent } from './core/layout/header/header.component';
+import { FooterComponent } from './core/layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
